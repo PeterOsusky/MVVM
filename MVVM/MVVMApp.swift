81219d00e466dd3eb4d63ed28dbb31fc39da205f
Peter Osusky <peterosusky94@gmail.com>
2023-10-25 14:45:55 +0200

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Peter on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
